352b551fef602373c6172b1d5bb2bd6edb231101
Kamil Młodzikowski <k.mlodzikowski@gmail.com>
2023-11-09 14:27:16 +0100

    added lab06 files

---
+++ Lab06-SLAM3D/arm_06_build.sh
@@ -0,0 +1,13 @@
+xhost +local:root
+
+# BUILD THE IMAGE
+ROS_IMAGE="arm/lab06"
+if [ -z "$1" ]
+	then
+		echo "RUNNING WITH 1 WORKER"
+		WORKERS=1
+	else
+		echo "RUNNING WITH $1 WORKERS"
+		WORKERS=$1
+fi
+docker build -f Dockerfile --build-arg parallel_workers=$WORKERS -t $ROS_IMAGE ./..
