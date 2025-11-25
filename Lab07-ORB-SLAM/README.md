8fd45a9258ac087d7cc130672b751d51885e02f0
Kamil Młodzikowski <k.mlodzikowski@gmail.com>
2025-11-25 17:41:44 +0100

    Add low-resource Dockerfile support and update build script for better resource management

--- Lab07-ORB-SLAM/README.md
+++ Lab07-ORB-SLAM/README.md
@@ -11,6 +11,13 @@ The ```arm_07_build.sh``` script accepts number of workers as an argument for pa
 bash ./arm_07_build.sh <number_of_workers>
 ```
 
+### Building on low-resource machines
+If the build regularly stalls due to RAM or CPU pressure, you can switch to a lighter Dockerfile that limits parallel compilation:
+```shell
+bash ./arm_07_build.sh <number_of_workers> --low-resource
+```
+When `--low-resource` is provided the script uses `Dockerfile_low`, which lowers the internal `make` parallelism for Pangolin/ORB-SLAM3 so that the container consumes significantly less memory (at the expense of build time).
+
 ## Running on Nvidia GPU
 ```shell
 wget https://raw.githubusercontent.com/kamilmlodzikowski/LabARM/main/Lab07-ORB-SLAM/arm_07_run_gpu_nvidia.sh -O arm_07_run_gpu_nvidia.sh
--- Lab07-ORB-SLAM/arm_07_build.sh
+++ Lab07-ORB-SLAM/arm_07_build.sh
@@ -2,12 +2,27 @@ xhost +local:root
 
 # BUILD THE IMAGE
 ROS_IMAGE="arm/lab07"
-if [ -z "$1" ]
-	then
-		echo "RUNNING WITH 1 WORKER"
-		WORKERS=1
-	else
-		echo "RUNNING WITH $1 WORKERS"
-		WORKERS=$1
-fi
-docker build --no-cache -f Dockerfile --build-arg parallel_workers=$WORKERS -t $ROS_IMAGE ./..
+DOCKERFILE="Dockerfile"
+WORKERS=1
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		-l|--low-resource)
+			echo "USING low-resource Dockerfile (Dockerfile_low)"
+			DOCKERFILE="Dockerfile_low"
+			shift
+			;;
+		-h|--help)
+			echo "Usage: $0 [workers] [--low-resource]"
+			exit 0
+			;;
+		*)
+			WORKERS="$1"
+			shift
+			;;
+	esac
+done
+
+echo "RUNNING WITH $WORKERS WORKER(S)"
+echo "Dockerfile: $DOCKERFILE"
+docker build --no-cache -f "$DOCKERFILE" --build-arg parallel_workers=$WORKERS -t $ROS_IMAGE ./..
